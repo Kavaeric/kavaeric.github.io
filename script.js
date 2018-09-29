2a8f4809e28452cb7da45a32c4636507813264fc
Kavaeric <kavaeric@gmail.com>
2018-09-29 17:49:15 -0400

    Image optimisations
    Compressed images; file size total down to ~7.6MB from ~19.5MB

--- script.js
+++ script.js
@@ -45,11 +45,12 @@ $(document).ready(function() {
             return;
         }
 
-        $('<style>#' + imageName + ' { background-image: url("img/' + imageName + '_smb.png"); }'
+        $('<style> #' + imageName + ' { background-image: url("img/' + imageName + '_smb.png"); }'
         +'.background.' + imageName + ' { background-image: url("img/' + imageName + '_smb.png"); }'
         +'.header.background.' + imageName + ' { background-image: url("img/' + imageName + '_sm.jpg"); }'
         +''
         +'@media only screen and (min-width: 768px) {'
+        +    '.background.' + imageName + ' { background-image: url("img/' + imageName + '_b.png"); }'
         +    '.header.background.' + imageName + ' { background-image: url("img/' + imageName + '.jpg"); }'
         +'}'
         +''
